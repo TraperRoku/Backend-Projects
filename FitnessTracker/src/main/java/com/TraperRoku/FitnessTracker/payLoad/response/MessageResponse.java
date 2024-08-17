8afd4c09f1b9d6d7caa1448dd5c937a367d9f2d7
Filip Kazmierczak <FilipKazmierczak@wp.pl>
2024-08-17 15:57:23 +0200

    JWT and loginRegister features

---
+++ FitnessTracker/src/main/java/com/TraperRoku/FitnessTracker/payLoad/response/MessageResponse.java
@@ -0,0 +1,17 @@
+package com.TraperRoku.FitnessTracker.payLoad.response;
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
