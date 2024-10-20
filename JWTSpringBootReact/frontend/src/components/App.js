a0d72601b7ca6f00c26548381eecd7ac276f890e
TraperRoku <119821555+TraperRoku@users.noreply.github.com>
2024-10-20 18:33:15 +0200

    Add files via upload

---
+++ JWTSpringBootReact/frontend/src/components/App.js
@@ -0,0 +1,20 @@
+import './App.css'
+import logo from '../logo.svg'
+import Header from './Header'
+import AppContent from './AppContent';
+
+function App(){
+    return (
+        <div>
+            <Header pageTitle="Json Web Token" logoSrc={logo} /> 
+           <div className="container-fluid">
+            <div className='row'>
+                <div className='col' >
+            <AppContent/>
+            </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+export default App;
