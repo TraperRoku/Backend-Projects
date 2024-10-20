4e2cfe4e9d3d0a1731f3c63652a8a72b5f5f1593
TraperRoku <119821555+TraperRoku@users.noreply.github.com>
2024-10-20 18:28:27 +0200

    Add files via upload

---
+++ JWTSpringBootReact/backend/src/main/java/com/TraperRoku/backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.TraperRoku.backend.repository;
+
+import com.TraperRoku.backend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByLogin(String login);
+}
