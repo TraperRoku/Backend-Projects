edfe791b9c3aa7ea53d255aac37fdfbf8fe55f68
TraperRoku <119821555+TraperRoku@users.noreply.github.com>
2024-12-01 18:59:21 +0100

    Add files via upload

---
+++ Recipe-Sharing-Platform/src/main/java/com/TraperRoku/Recipe/Sharing/Platform/RecipeSharingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.TraperRoku.Recipe.Sharing.Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSharingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSharingPlatformApplication.class, args);
+	}
+
+}
