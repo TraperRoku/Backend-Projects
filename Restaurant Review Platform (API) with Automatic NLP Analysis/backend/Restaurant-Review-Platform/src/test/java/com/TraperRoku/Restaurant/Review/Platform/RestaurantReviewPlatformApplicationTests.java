f623aaa9e78f4a9152fee83993ea7349a6223b7d
Filip Kazmierczak <FilipKazmierczak@wp.pl>
2025-03-06 00:53:40 +0100

    mongdb

---
+++ Restaurant Review Platform (API) with Automatic NLP Analysis/backend/Restaurant-Review-Platform/src/test/java/com/TraperRoku/Restaurant/Review/Platform/RestaurantReviewPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TraperRoku.Restaurant.Review.Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReviewPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
